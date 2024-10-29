e1b6214cbff9abe701cf03e3e587429db5926b52
jonatas <jonatas_dev@hotmail.com>
2024-10-29 17:16:46 -0300

    Operações matematicas em Java

--- src/Main.java
+++ src/Main.java
@@ -79,5 +79,31 @@ public class Main {
         j = sc.next().charAt(0);//Para ler o primeiro caracter de uma String.
         System.out.println("Você digitou: " + j);
         sc.close();
+
+        //Op Matematicas
+
+        double x = 3.0;
+        double y = 4.0;
+        double z = -5.0;
+        double A, B, C;
+        //Raiz quadrada de um número Math.sqrt();
+        A = Math.sqrt(x);
+        B = Math.sqrt(y);
+        C = Math.sqrt(25.0);
+        System.out.println("Raiz quadrada de " + x + " = " + A);
+        System.out.println("Raiz quadrada de " + y + " = " + B);
+        System.out.println("Raiz quadrada de 25 = " + C);
+        //Valor elevado de um número Math.pow();
+        A = Math.pow(x, y);
+        B = Math.pow(x, 2.0);
+        C = Math.pow(5.0, 2.0);
+        System.out.println(x + " elevado a " + y + " = " + A);
+        System.out.println(x + " elevado ao quadrado = " + B);
+        System.out.println("5 elevado ao quadrado = " + C);
+        //Valor absoluto de um número Math.abs();
+        A = Math.abs(y);
+        B = Math.abs(z);
+        System.out.println("Valor absoluto de " + y + " = " + A);
+        System.out.println("Valor absoluto de " + z + " = " + B);
     }
 }
